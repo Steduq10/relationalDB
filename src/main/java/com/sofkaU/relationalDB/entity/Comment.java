81c5958e58eb7fb724170427e45ec4493e45a8ec
Steven Duque <steduq10@DESKTOP-RCPLA2L.localdomain>
2022-07-20 17:30:32 -0500

    Post and Post entities created

---
+++ src/main/java/com/sofkaU/relationalDB/entity/Comment.java
@@ -0,0 +1,18 @@
+package com.sofkaU.relationalDB.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity(name = "Comment")
+@Table(name = "comment")
+@Data
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String message;
+    private Long FK_post_id;
+
+}
